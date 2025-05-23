71175f8547b3bd9cbb6f8cc3d83de9cbaebd139e
tmead706 <tmead706@gmail.com>
2025-05-23 03:42:16 -0400

    Create LabelBridge.java

---
+++ LabelBridge.java
@@ -0,0 +1,23 @@
+package sml;
+
+/*
+ * This class represents the mechanism for accessing a label 
+ * given that there is restricted access to the class
+ *
+ * @author KLM and xxx
+ */
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import static lombok.AccessLevel.PUBLIC;
+
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor(access = PUBLIC)
+
+public class LabelBridge {
+    // TODO
+
+
+}
